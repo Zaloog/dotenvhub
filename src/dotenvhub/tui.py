d0fd9076613841e69d6c2dc9306bf36c7e41b0e4
Zaloog <gramslars@gmail.com>
2024-01-14 11:56:21 +0100

    Export Functionality Added also Initial no highlight

--- src/dotenvhub/tui.py
+++ src/dotenvhub/tui.py
@@ -18,7 +18,7 @@ from textual.widgets import (
 )
 
 from .constants import ENV_FILE_DIR_PATH
-from .utils import copy_path_to_clipboard, get_env_content
+from .utils import copy_path_to_clipboard, create_copy_in_cwd, get_env_content
 
 # Fragen
 # Do Updates happen in Displayed UI based on File System Changes
@@ -37,34 +37,51 @@ class EnvFileSelector(VerticalScroll):
         self.build_selector()
         for dirpath, filenames in self.dir_paths.items():
             if dirpath == Path("."):
-                yield ListView(
+                general_list = ListView(
                     *[
                         ListItem(Label(f":page_facing_up: {file}"), id=file)
                         for file in filenames
                     ],
+                    initial_index=None,
                 )
+                yield general_list
             else:
-                yield Collapsible(
-                    ListView(
-                        *[
-                            ListItem(Label(f":page_facing_up: {file}"), id=file)
-                            for file in filenames
-                        ],
-                        id=str(dirpath),
-                    ),
+                folder_list = ListView(
+                    *[
+                        ListItem(Label(f":page_facing_up: {file}"), id=file)
+                        for file in filenames
+                    ],
+                    id=str(dirpath),
+                    initial_index=None,
+                )
+                folder_colabs = Collapsible(
+                    folder_list,
                     title=f"{dirpath}",
                     collapsed_symbol=":file_folder:",
                     expanded_symbol=":open_file_folder:",
                 )
+                yield folder_colabs
 
 
 class FilePreviewer(TextArea):
     pass
 
 
+class InteractionPanel(Container):
+    def compose(self):
+        yield Button.warning("Create Shell String", id="shell_export_btn")
+        yield Button.warning("Export File to current dir", id="export_btn")
+        yield Button.warning("Copy Path to Clipboard", id="clipboard_path_btn")
+        yield Label("export filename")
+        yield Input(
+            value=".env", placeholder="env file name for export", id="export-env-name"
+        )
+
+
 class DotEnvHub(App):
     CSS_PATH = "./assets/tui.css"
 
+    file_to_show = var("")
     file_to_show_path = var("")
     text_to_display = var("")
 
@@ -78,23 +95,20 @@ class DotEnvHub(App):
 
             with Horizontal(id="file-preview"):
                 # Add File Name Display
-                yield FilePreviewer(id="text-preview")
+                fp = FilePreviewer(id="text-preview")
+                fp.border_title = self.file_to_show_path
+                yield fp
 
-            with Container(id="interaction"):
-                yield Button.warning("Create Shell String", id="shell_export_btn")
-                yield Button.warning("Export File to current dir", id="export_btn")
-                yield Button.warning("Copy Path to Clipboard", id="clipboard_path_btn")
-                yield Label("export filename")
-                yield Input(placeholder="env name", id="bottom-right-final")
+            yield InteractionPanel(id="interaction")
 
     @on(ListView.Selected)
     def preview_file(self, event: ListView.Selected):
-        file = Path(event.list_view.highlighted_child.id)
+        self.file_to_show = Path(event.list_view.highlighted_child.id)
         if event.list_view.id:
             folder = Path(event.list_view.id)
-            self.file_to_show_path = ENV_FILE_DIR_PATH / folder / file
+            self.file_to_show_path = ENV_FILE_DIR_PATH / folder / self.file_to_show
         else:
-            self.file_to_show_path = ENV_FILE_DIR_PATH / file
+            self.file_to_show_path = ENV_FILE_DIR_PATH / self.file_to_show
         log(self.file_to_show_path)
 
     @on(ListView.Selected)
@@ -107,9 +121,15 @@ class DotEnvHub(App):
         log(self.text_to_display)
 
     @on(Button.Pressed, "#clipboard_path_btn")
-    def current_env_to_clipboard(self):
+    def copy_env_path(self):
         copy_path_to_clipboard(path=self.file_to_show_path)
         log("copied file path to clipboard")
+
+    @on(Button.Pressed, "#export_btn")
+    def export_env_file(self):
+        export_filename = self.query_one(Input).value
+        create_copy_in_cwd(filename=export_filename, filepath=self.file_to_show_path)
+        log("created Export file")
 
 
 myapp = DotEnvHub()
--- src/dotenvhub/utils.py
+++ src/dotenvhub/utils.py
@@ -1,3 +1,6 @@
+import shutil
+from pathlib import Path
+
 import pyperclip
 
 
@@ -5,6 +8,11 @@ def copy_path_to_clipboard(path):
     pyperclip.copy(str(path))
 
 
-def get_env_content(filepath):
+def get_env_content(filepath: Path):
     with open(filepath, "r") as env_file:
         return "".join(env_file.readlines())
+
+
+def create_copy_in_cwd(filename: str, filepath: Path):
+    cwd = Path.cwd()
+    shutil.copy(filepath, cwd / filename)
