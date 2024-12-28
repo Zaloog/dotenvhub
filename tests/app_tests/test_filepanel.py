92c23e72a6c28e23616213550e4d9f3a2153758e
Zaloog <gramslars@gmail.com>
2024-12-28 15:24:41 +0100

    Add Test for created folders

--- tests/app_tests/test_filepanel.py
+++ tests/app_tests/test_filepanel.py
@@ -14,8 +14,6 @@ APP_SIZE = (120, 80)
     ],
 )
 async def test_filepanel_file(test_app, test_data_path, file_list, amount):
-    # (test_data_path / 'test123').touch()
-    # assert (test_data_path/'test123').exists()
     for file in file_list:
         if file:
             (test_data_path / file).touch()
@@ -27,4 +25,48 @@ async def test_filepanel_file(test_app, test_data_path, file_list, amount):
             assert (
                 pilot.app.file_selector.query(ListItem).first().file_name
                 == file_list[0]
+            )
+
+
+@pytest.mark.parametrize(
+    "file_list, folder_list, amount",
+    [
+        ([], [], 0),
+        (["test1"], ["."], 1),
+        (["test1", "test2"], [".", "folder1"], 4),
+        (["test1", "test2", "test3"], ["folder1", "folder2"], 6),
+    ],
+)
+async def test_filepanel_folder(
+    test_app, test_data_path, file_list, folder_list, amount
+):
+    # create files
+    for folder in folder_list:
+        if not folder:
+            folder_path = None
+        elif folder == ".":
+            folder_path = test_data_path
+        else:
+            folder_path = test_data_path / folder
+            if not folder_path.exists():
+                folder_path.mkdir(exist_ok=True)
+
+        for file in file_list:
+            if file:
+                (folder_path / file).touch()
+                assert (folder_path / file).exists()
+
+    # test if files are present in file_selector
+    async with test_app.run_test(size=APP_SIZE) as pilot:
+        assert len(list(pilot.app.file_selector.query(ListItem))) == amount
+        if file_list:
+            assert (
+                pilot.app.file_selector.query(ListItem).first().file_name
+                == file_list[0]
+            )
+
+        if folder_list and (folder_list[0] != "."):
+            assert (
+                pilot.app.file_selector.query(ListItem).first().dir_name
+                == folder_list[1]  # ordered descending
             )
